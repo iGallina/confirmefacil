ce068109c5b1f181ab10b47e047d069b80a0a2f7
edhana <edhana@gmail.com>
2011-02-20 18:19:09 -0300

    Arrumando um primeiro teste

--- spec/controllers/confirmations_controller_spec.rb
+++ spec/controllers/confirmations_controller_spec.rb
@@ -1,5 +1,8 @@
-require 'spec_helper'
+require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 
 describe ConfirmationsController do
+    it 'should do something' do
+        
+    end
 
 end
