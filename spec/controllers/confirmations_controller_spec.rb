253355d4d0866967270f6c0d2a637a81a2174243
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-20 18:54:07 -0300

    dois testes passando pro controller de confirmação

--- spec/controllers/confirmations_controller_spec.rb
+++ spec/controllers/confirmations_controller_spec.rb
@@ -3,15 +3,23 @@ require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 describe ConfirmationsController do
     it 'should return no appointments when there is no appointment' do
         confirmations = ConfirmationsController.new
-        confirmations.get_appointments.length.should == 0
+        confirmations.get_unconfirmed_appointments.length.should == 0
     end
 
 
     it 'should return an appointment when it is not confirmed' do
         confirmations = ConfirmationsController.new
         
+        appointment = Appointment.new
+		appointment.doctor = "rolim"
+		appointment.client = "client"
+		appointment.celular = "81050561"
+		appointment.status = 0
+		
+		appointment.save
         
-        confirmations.get_appointments.length.should == 0
+        confirmations.get_unconfirmed_appointments.length.should == 1
+        confirmations.get_unconfirmed_appointments[0].doctor == "rolim"
     end
 
 end
