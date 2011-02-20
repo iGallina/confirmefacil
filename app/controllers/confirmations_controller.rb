1f0e8a2f2f4912786f771d988a5deff866d3fc74
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-20 19:34:27 -0300

    pesquisa de consultas nao confirmadas finalizada e testada

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,9 @@
 class ConfirmationsController < ApplicationController
 
 	def get_unconfirmed_appointments
-		appointments = Appointment.all
+		today = Date.today.midnight
+		tomorrow = Date.tomorrow.midnight
+		appointments = Appointment.find(:all, :conditions=>["status=? and date>=? and date<? ", Appointment::SENT, today, tomorrow])
 		return appointments
 	end
 
