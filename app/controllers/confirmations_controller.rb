8f7fe012af60be97f78fd6bee27619046524f7ca
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-20 20:43:24 -0300

    esqeuma da partial de confirmações tá pronto

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,8 +3,11 @@ class ConfirmationsController < ApplicationController
 	def get_unconfirmed_appointments
 		today = Date.today.midnight
 		tomorrow = Date.tomorrow.midnight
-		appointments = Appointment.find(:all, :conditions=>["status=? and date>=? and date<? ", Appointment::SENT, today, tomorrow])
-		return appointments
+		@unconfirmed_appointments = Appointment.find(:all, :conditions=>["status=? and date>=? and date<? ", Appointment::SENT, today, tomorrow])
+
+		respond_to do |format|
+            format.js #{render  "get_unconfirmed_appointments.js.erb"}
+        end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ ConfirmeFacil::Application.routes.draw do
   resources :appointments
   resources :confirmations
 
+  match "get_unconfirmed_appointments" => "confirmations#get_unconfirmed_appointments"
+
   root :to => "sms_manager#index"
 
 end
