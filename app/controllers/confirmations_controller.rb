09e1c317aa6d47f97c673c05aa1e6f37926a78d4
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-20 18:30:14 -0300

    layout reestabelecido

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,2 +1,7 @@
 class ConfirmationsController < ApplicationController
+
+	def get_appointments
+		return []
+	end
+
 end
--- spec/controllers/confirmations_controller_spec.rb
+++ spec/controllers/confirmations_controller_spec.rb
@@ -1,8 +1,17 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 
 describe ConfirmationsController do
-    it 'should do something' do
+    it 'should return no appointments when there is no appointment' do
+        confirmations = ConfirmationsController.new
+        confirmations.get_appointments.length.should == 0
+    end
+
+
+    it 'should return an appointment when it is not confirmed' do
+        confirmations = ConfirmationsController.new
         
+        
+        confirmations.get_appointments.length.should == 0
     end
 
 end
